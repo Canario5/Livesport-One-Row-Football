d3589a4855445f4ae8f6452cbcedc80ab0e66c65
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-04-05 15:47:08 +0200

    clickReset() no longer needed

--- scripts/bettersport.js
+++ scripts/bettersport.js
@@ -1,10 +1,4 @@
-import { matchRefresh } from "/scripts/matchRefresh.js" // rearranging match scores
-import { clickReset } from "/scripts/clickReset.js" // helps with moving between subpages (All, Odds, Played...)
-
-export const nrMatches = (reset) => {
-	const nr = reset ? 0 : document.querySelectorAll(".event__match").length
-	return nr
-}
+import { matchRefresh } from "/scripts/matchRefresh.js"
 
 const deepObserving = () => {
 	deepObserver.observe(document.body, {
@@ -16,7 +10,6 @@ const deepObserving = () => {
 const deepObserver = new MutationObserver(() => {
 	if (document.querySelectorAll(".event__match").length) {
 		deepObserver.disconnect()
-		clickReset(".filters")
 
 		fullRefresh()
 		shortObserving()
@@ -31,13 +24,6 @@ const shortObserving = () => {
 }
 
 const shortObserver = new MutationObserver((mutations) => {
-	/* Not perfect, there can be a same number of matches on different tabs like Live or Played. 
-	Solved by clickReset.js */
-	if (document.querySelectorAll(".event__match").length !== nrMatches()) {
-		fullRefresh()
-	}
-
-	/* Checking for started/resumed games after loading of the page */
 	for (const mutation of mutations) {
 		if (mutation.addedNodes.length <= 0) continue
 
@@ -51,9 +37,9 @@ const shortObserver = new MutationObserver((mutations) => {
 
 		if (mutation.addedNodes[0].classList.contains("event__stage")) {
 			matchRefresh(mutation.target, true) // refresh all types of match (with not-live match as well) because of PKV starts
+			continue
 		}
 
-		// When a user is not logged !localStorage.hasOwnProperty("lsid_id")
 		if (mutation.target.classList.contains("sportName")) {
 			fullRefresh()
 			break
