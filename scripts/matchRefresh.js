2f78bedadf95f8cf2ad1da7586704859053da888
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-04-05 15:48:33 +0200

    Splited into two js files

--- scripts/matchRefresh.js
+++ scripts/matchRefresh.js
@@ -1,4 +1,4 @@
-import { initClasses } from "/scripts/initClasses.js"
+import { transformScore } from "/scripts/transformScore.js"
 
 export { matchRefresh }
 
@@ -16,53 +16,19 @@ const matchRefresh = (match, everyMatch) => {
 	/* For single match refresh with every match (not currently active included) */
 	if (eleScoreContainer && everyMatch) {
 		scoreNodes = eleScoreContainer
-		scoreInit(match, scoreNodes)
+		return transformScore(match, scoreNodes)
 	}
 
 	/* For all matches refresh, but only live matches refreshed */
 	if (eleScoreContainer && !everyMatch && match.classList.contains("event__match--live")) {
 		scoreNodes = eleScoreContainer
-		scoreInit(match, scoreNodes)
+		return transformScore(match, scoreNodes)
 	}
 
 	if (!eleScoreContainer) {
 		const divScore = document.createElement("div")
 		divScore.className = "scoreContainer"
 		scoreNodes = scoreNodes.appendChild(divScore)
-		scoreInit(match, scoreNodes)
-	}
-
-	if (scoreNodes) {
-		match.appendChild(scoreNodes)
-	}
-}
-
-const scoreInit = (match, scoreNodes) => {
-	const { homeClasses, awayClasses, colonClasses, editHalf } = initClasses()
-
-	// prettier-ignore
-	homeClasses.map((homeClass, i) =>
-		match.querySelector(`:scope > ${homeClass}`)
-			? transformScore(homeClasses, awayClasses[i], colonClasses[i], editHalf[i], match, scoreNodes)
-			: undefined
-	)
-}
-
-const transformScore = (home, away, colonClass, editHalf, match, scoreNodes) => {
-	const scoreHome = match.querySelector(home)
-	const scoreAway = match.querySelector(away)
-	const scoreColon = document.createElement("span")
-
-	scoreNodes.appendChild(scoreHome)
-	scoreNodes.appendChild(scoreColon)
-	scoreNodes.appendChild(scoreAway)
-
-	scoreColon.className = colonClass
-	scoreColon.textContent = ":"
-
-	// prettier-ignore
-	if (editHalf) {
-		scoreHome.textContent = scoreHome.textContent.substring(0, scoreHome.textContent.length - 1)
-		scoreAway.textContent = scoreAway.textContent.substring(1, scoreAway.textContent.length)
+		return transformScore(match, scoreNodes)
 	}
 }
