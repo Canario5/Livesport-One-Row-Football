4b70156bea76bbded3e0caa9f275d7de58022dfd
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-04-05 15:49:21 +0200

    Create transformScore.js

---
+++ scripts/transformScore.js
@@ -0,0 +1,36 @@
+import { initClasses } from "/scripts/initClasses.js"
+
+export { transformScore }
+
+const transformScore = (match, scoreNodes) => {
+	const { homeClasses, awayClasses, colonClasses, editHalf } = initClasses()
+
+	homeClasses.map((homeClass, i) =>
+		match.querySelector(`:scope > ${homeClass}`)
+			? scoreItem(homeClasses, awayClasses[i], colonClasses[i], editHalf[i])
+			: undefined
+	)
+
+	function scoreItem(home, away, colonClass, editHalf) {
+		const scoreHome = match.querySelector(home)
+		const scoreAway = match.querySelector(away)
+		const scoreColon = document.createElement("span")
+
+		scoreNodes.appendChild(scoreHome)
+		scoreNodes.appendChild(scoreColon)
+		scoreNodes.appendChild(scoreAway)
+
+		scoreColon.className = colonClass
+		scoreColon.textContent = ":"
+
+		// prettier-ignore
+		if (editHalf) {
+			scoreHome.textContent = scoreHome.textContent.substring(0, scoreHome.textContent.length - 1)
+			scoreAway.textContent = scoreAway.textContent.substring(1, scoreAway.textContent.length)
+		}
+	}
+
+	if (scoreNodes.hasChildNodes()) {
+		match.appendChild(scoreNodes)
+	}
+}
